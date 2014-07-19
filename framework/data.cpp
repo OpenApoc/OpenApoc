8a80f694f2e36010b97b22d7c2de8310f6b5d662
Jonathan Hamilton <jonathan.hamilton@imgtec.com>
2014-07-19 14:24:12 -0700

    Data: If we fail to load a file, try a lower-case version
    Means I don't have to play around with filenames in Linux as the CD
    iso mounts with lowercase filenames by default

--- framework/data.cpp
+++ framework/data.cpp
@@ -2,6 +2,7 @@
 
 #include <cassert>
 #include <iostream>
+#include <algorithm>
 
 Data *Data::data = nullptr;
 
@@ -21,12 +22,21 @@ Data::load_bitmap(const std::string path)
 {
 	std::string fullpath = std::string(this->root + this->DIR_SEP + path);
 	auto bitmap = al_load_bitmap(fullpath.c_str());
-	if (!bitmap)
-	{
-		std::cerr << "Failed to load \"" + fullpath + "\"\n";
-		assert(0);
-	}
-	return bitmap;
+	if (bitmap)
+		return bitmap;
+
+	/* Try lowercase version: */
+	auto lowerpath = path;
+	std::transform(lowerpath.begin(), lowerpath.end(), lowerpath.begin(),  ::tolower);
+	fullpath = std::string(this->root + this->DIR_SEP + lowerpath);
+	std::cerr << "Failed to load \"" + path + "\", trying \"" + lowerpath + "\"\n";
+	bitmap = al_load_bitmap(fullpath.c_str());
+	if (bitmap)
+		return bitmap;
+
+	std::cerr << "Failed to load \"" + path + "\"\n";
+	assert(0);
+	return nullptr;
 }
 
 ALLEGRO_FILE *
@@ -34,10 +44,20 @@ Data::load_file(const std::string path, const char *mode)
 {
 	std::string fullpath = std::string(this->root + this->DIR_SEP + path);
 	auto file = al_fopen(fullpath.c_str(), mode);
-	if (!file)
-	{
-		std::cerr << "Failed to load \"" + fullpath + "\"\n";
-		assert(0);
-	}
-	return file;
+	if (file)
+		return file;
+
+	/* Try lowercase version: */
+	auto lowerpath = path;
+	std::transform(lowerpath.begin(), lowerpath.end(), lowerpath.begin(),  ::tolower);
+	fullpath = std::string(this->root + this->DIR_SEP + lowerpath);
+	std::cerr << "Failed to load \"" + path + "\", trying \"" + lowerpath + "\"\n";
+	file = al_fopen(fullpath.c_str(), mode);
+	if (file)
+		return file;
+
+	std::cerr << "Failed to load \"" + path + "\"\n";
+	assert(0);
+
+	return nullptr;
 }
