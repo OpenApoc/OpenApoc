955db81bebbc6cdd4045b5ed329f156ce4b06ddf
Jonathan Hamilton <jtrhamilton@gmail.com>
2014-10-01 15:16:53 -0700

    Add initial mouse selection
    TODO:
    - Go down z stack to find first non-empty (rather than just the current
      'maxZ' rendered)
    - Change selection hit box? (Currently {0,0,TILE_WIDTH/2,TILE_HEIGHT/2}
      which maps to the top-left of each tile)

--- game/city/cityview.cpp
+++ game/city/cityview.cpp
@@ -1,5 +1,6 @@
 #include "cityview.h"
 #include "city.h"
+#include "../../library/vec.h"
 
 #include <iostream>
 
@@ -30,6 +31,25 @@ void CityView::Resume()
 
 void CityView::Finish()
 {
+}
+
+
+static Vec2<int> translateCityToScreenCoords(Vec3<int> c)
+{
+	int x = (c.x*CITY_TILE_WIDTH/2) - (c.y*CITY_TILE_WIDTH/2);
+	int y = (c.x*CITY_TILE_HEIGHT/2) + (c.y*CITY_TILE_HEIGHT/2)
+		+ (c.z*CITY_TILE_ZOFFSET);
+
+	return Vec2<int>{x,y};
+}
+
+static Vec3<int> translateScreenToCityCoords(Vec2<int> screenPos, int z)
+{
+	screenPos.y -= (z*CITY_TILE_ZOFFSET);
+	int y = ((screenPos.y/(CITY_TILE_HEIGHT/2)) - (screenPos.x/(CITY_TILE_WIDTH/2))) / (2);
+	int x = ((screenPos.y/(CITY_TILE_HEIGHT/2)) + (screenPos.x/(CITY_TILE_WIDTH/2))) / (2);
+
+	return Vec3<int>{x,y,z};
 }
 
 void CityView::EventOccurred(Event *e)
@@ -104,6 +124,18 @@ void CityView::EventOccurred(Event *e)
 				break;
 		}
 	}
+	else if (e->Type == EVENT_MOUSE_DOWN)
+	{
+		auto &ev = e->Data.Mouse;
+		auto selected = translateScreenToCityCoords(Vec2<int>{ev.X - offsetX, ev.Y - offsetY}, maxZDraw-1);
+
+		if (selected.x < 0) selected.x = 0;
+		if (selected.y < 0) selected.y = 0;
+		if (selected.x > 99) selected.x = 99;
+		if (selected.y > 99) selected.y = 99;
+		selectedTilePosition = selected;
+		selectionChanged = true;
+	}
 	if (GAMECORE->DebugModeEnabled &&
 	    selectionChanged)
 	{
@@ -149,27 +181,22 @@ void CityView::Render()
 
 				auto &tile = CITY->tiles[z][y][x];
 				// Skip over transparent (missing) tiles
-				int posX = offsetX
-					+ (x*CITY_TILE_WIDTH/2)
-					- (y*CITY_TILE_WIDTH/2);
-				int posY = offsetY
-					+ (x*CITY_TILE_HEIGHT/2)
-					+ (y*CITY_TILE_HEIGHT/2)
-					+ (z*CITY_TILE_ZOFFSET);
-
+				auto screenPos = translateCityToScreenCoords(Vec3<int>{x,y,z});
+				screenPos.x += offsetX;
+				screenPos.y += offsetY;
 				//Skip over tiles that would be outside the window
-				if (posX + CITY_TILE_WIDTH < 0 || posY + CITY_TILE_HEIGHT < 0
-					|| posX - CITY_TILE_WIDTH > dpyWidth || posY - CITY_TILE_HEIGHT > dpyHeight)
+				if (screenPos.x + CITY_TILE_WIDTH < 0 || screenPos.y + CITY_TILE_HEIGHT < 0
+					|| screenPos.x - CITY_TILE_WIDTH > dpyWidth || screenPos.y - CITY_TILE_HEIGHT > dpyHeight)
 					continue;
 
 				if (showSelected)
-					selectedTileImageBack->draw(posX, posY);
+					selectedTileImageBack->draw(screenPos.x, screenPos.y);
 
 				if (tile.id != 0)
-					cityPck->RenderImage(tile.id, posX, posY);
+					cityPck->RenderImage(tile.id, screenPos.x, screenPos.y);
 
 				if (showSelected)
-					selectedTileImageFront->draw(posX, posY);
+					selectedTileImageFront->draw(screenPos.x, screenPos.y);
 			}
 		}
 	}
