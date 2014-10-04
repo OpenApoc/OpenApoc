4c06b7641f731f6a5bef749717a354c9f325cd0f
Jonathan Hamilton <jtrhamilton@gmail.com>
2014-10-04 16:04:34 -0700

    Remove unused network support, remove unused event AdditionalData

--- framework/event.cpp
+++ framework/event.cpp
@@ -11,34 +11,6 @@ Event::Event()
 
 Event::~Event()
 {
-	if( Type == EVENT_FORM_INTERACTION )
-	{
-		if( Data.Forms.AdditionalData != nullptr )
-		{
-			delete Data.Forms.AdditionalData;
-		}
-	}
-
-#ifdef NETWORK_SUPPORT
-	if( Type == EVENT_NETWORK_PACKET_RECEIVED )
-	{
-		enet_packet_destroy( Data.Network.Traffic.packet );
-	}
-#endif
-
-#ifdef DOWNLOAD_SUPPORT
-	if( Type == EVENT_DOWNLOAD_COMPLETE || Type == EVENT_DOWNLOAD_PROGRESS )
-	{
-		if( URL != 0 )
-		{
-			delete URL;
-		}
-		if( Contents != 0 )
-		{
-			delete Contents;
-		}
-	}
-#endif
 
 }
 
