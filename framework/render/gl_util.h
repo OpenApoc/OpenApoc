a105d998ae9240a7d3566d6624964bff011ef6b4
Jonathan Hamilton <jtrhamilton@gmail.com>
2015-01-27 12:52:52 -0800

    Quick awful VAO use
    Required for GL3.3 core, as part of moving to core for instance/array
    textures etc.

--- framework/render/gl_util.h
+++ framework/render/gl_util.h
@@ -15,23 +15,47 @@ namespace GL
 class IdentityQuad
 {
 public:
-	static void draw(GLint positionAttribute)
+	static GLuint vbo;
+	static GLuint vao;
+
+	static void setup_vbo()
 	{
-		static float vertices[] =
+		static const float vertices[] =
 		{
 			0.0f, 0.0f,
 			0.0f, 1.0f,
 			1.0f, 0.0f,
 			1.0f, 1.0f,
 		};
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glGenVertexArrays(1, &vao);
+
+	}
+
+	static void draw(GLint positionAttribute)
+	{
+		if (!vbo)
+			setup_vbo();
+
+		glBindVertexArray(vao);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		glVertexAttribPointer(positionAttribute, 2, GL_FLOAT, GL_FALSE, 0, &vertices);
+		glVertexAttribPointer(positionAttribute, 2, GL_FLOAT, GL_FALSE, 0, NULL);
 		glEnableVertexAttribArray(positionAttribute);
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 		glDisableVertexAttribArray(positionAttribute);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
 	}
 };
+
+GLuint IdentityQuad::vbo = 0;
+GLuint IdentityQuad::vao = 0;
 
 static void debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam)
 {
