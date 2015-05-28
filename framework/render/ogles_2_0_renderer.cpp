9c9abce8a7dca75041ebdb252ad916f003f31ccf
Jonathan Hamilton <jtrhamilton@gmail.com>
2015-05-27 20:13:30 -0700

    Add non-functional stub GLESv2 renderer

--- framework/render/ogles_2_0_renderer.cpp
+++ framework/render/ogles_2_0_renderer.cpp
@@ -10,6 +10,100 @@ namespace {
 
 using namespace OpenApoc;
 
+class OGLES20Renderer : public Renderer
+{
+private:
+public:
+	virtual ~OGLES20Renderer()
+	{
+	}
+	virtual void setSurface(std::shared_ptr<Surface> s)
+	{
+		std::ignore = s;
+		LogError("UNIMPLEMENTED");
+	}
+	virtual std::shared_ptr<Surface> getSurface()
+	{
+		LogError("UNIMPLEMENTED");
+		return nullptr;
+	}
+	virtual void clear(Colour c = Colour{0,0,0,0})
+	{
+		std::ignore = c;
+		LogError("UNIMPLEMENTED");
+	}
+	virtual void setPalette(std::shared_ptr<Palette> p)
+	{
+		std::ignore = p;
+		LogError("UNIMPLEMENTED");
+	}
+	virtual void draw(std::shared_ptr<Image> i, Vec2<float> position)
+	{
+		std::ignore = i;
+		std::ignore = position;
+		LogError("UNIMPLEMENTED");
+	}
+	virtual void drawRotated(std::shared_ptr<Image> i, Vec2<float> center, Vec2<float> position, float angle)
+	{
+		std::ignore = i;
+		std::ignore = center;
+		std::ignore = position;
+		std::ignore = angle;
+		LogError("UNIMPLEMENTED");
+	}
+	virtual void drawScaled(std::shared_ptr<Image> i, Vec2<float> position, Vec2<float> size, Scaler scaler = Scaler::Linear)
+	{
+		std::ignore = i;
+		std::ignore = position;
+		std::ignore = size;
+		std::ignore = scaler;
+		LogError("UNIMPLEMENTED");
+	}
+	virtual void drawTinted(std::shared_ptr<Image> i, Vec2<float> position, Colour tint)
+	{
+		std::ignore = i;
+		std::ignore = position;
+		std::ignore = tint;
+		LogError("UNIMPLEMENTED");
+	}
+	virtual void drawFilledRect(Vec2<float> position, Vec2<float> size, Colour c)
+	{
+		std::ignore = position;
+		std::ignore = size;
+		std::ignore = c;
+		LogError("UNIMPLEMENTED");
+	}
+	virtual void drawRect(Vec2<float> position, Vec2<float> size, Colour c, float thickness = 1.0)
+	{
+		std::ignore = position;
+		std::ignore = size;
+		std::ignore = c;
+		std::ignore = thickness;
+		LogError("UNIMPLEMENTED");
+	}
+	virtual void drawLine(Vec2<float> p1, Vec2<float> p2, Colour c, float thickness = 1.0)
+	{
+		std::ignore = p1;
+		std::ignore = p2;
+		std::ignore = c;
+		std::ignore = thickness;
+		LogError("UNIMPLEMENTED");
+	}
+	virtual void flush()
+	{
+		LogError("UNIMPLEMENTED");
+	}
+	virtual UString getName()
+	{
+		return "OpenGL|ES 2.0 renderer";
+	}
+	virtual std::shared_ptr<Surface> getDefaultSurface()
+	{
+		LogError("UNIMPLEMENTED");
+		return nullptr;
+	}
+};
+
 class OGLES20RendererFactory : public OpenApoc::RendererFactory
 {
 public:
@@ -37,8 +131,7 @@ public:
 			Strings::ToInteger(splitVersion[2]) >= 2)
 		{
 			LogInfo("Valid OpenGLES2+ context found");
-			//return new GLES20Renderer();
-			return nullptr;
+			return new OGLES20Renderer();
 		}
 		LogInfo("Not a GLESv2+ context - trying to find GLES2 compatibility extension");
 
@@ -48,8 +141,7 @@ public:
 			if (e == "GL_ARB_ES2_compatibility")
 			{
 				LogInfo("Found GL_ARB_ES2_compatibility extension");
-				//return new GLES20Renderer();
-				return nullptr;
+				return new OGLES20Renderer();
 			}
 		}
 		return nullptr;
