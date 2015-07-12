9f7fb4719475c29c810c7d56bf7eefc1fe5db63d
Jonathan Hamilton <jtrhamilton@gmail.com>
2015-07-12 14:43:52 -0700

    Re-add testing vehicles
    Add some variety (phoenix hovercars /and/ police hovercars!) while I'm
    at it too

--- game/gamestate.cpp
+++ game/gamestate.cpp
@@ -2,6 +2,7 @@
 #include "game/city/city.h"
 
 #include "framework/includes.h"
+#include "framework/framework.h"
 
 namespace OpenApoc {
 
@@ -13,6 +14,58 @@ GameState::GameState(Framework &fw, Rules &rules)
 	}
 
 	this->city.reset(new City(fw));
+	//Place some random testing vehicles
+	std::default_random_engine generator;
+	//FIXME: Read size from city? (Only 'temporary' code anyway)
+	std::uniform_int_distribution<int> xydistribution(0,99);
+	// Spawn vehicles at the top to avoid creating them in small
+	// inescapeable gaps in buildings
+	std::uniform_int_distribution<int> zdistribution(8,9);
+
+	for (int i = 0; i < 50; i++)
+	{
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		do {
+			x = xydistribution(generator);
+			y = xydistribution(generator);
+			z = zdistribution(generator);
+		} while(!this->city->getTile(x,y,z)->ownedObjects.empty());
+
+		auto vehicleDefIt = fw.rules->getVehicleDefs().find("POLICE_HOVERCAR");
+		if (vehicleDefIt == fw.rules->getVehicleDefs().end())
+		{
+			LogError("No POLICE_HOVERCAR vehicle def found?");
+			return;
+		}
+		//Organisations[3] == megapol
+		auto testVehicle = std::make_shared<Vehicle>(vehicleDefIt->second, this->organisations[3]);
+		this->city->vehicles.push_back(testVehicle);
+		testVehicle->launch(*this->city, Vec3<float>{x,y,z});
+	}
+	for (int i = 0; i < 50; i++)
+	{
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		do {
+			x = xydistribution(generator);
+			y = xydistribution(generator);
+			z = zdistribution(generator);
+		} while(!this->city->getTile(x,y,z)->ownedObjects.empty());
+
+		auto vehicleDefIt = fw.rules->getVehicleDefs().find("PHOENIX_HOVERCAR");
+		if (vehicleDefIt == fw.rules->getVehicleDefs().end())
+		{
+			LogError("No PHOENIX_HOVERCAR vehicle def found?");
+			return;
+		}
+		//Organisations[3] == X-Com
+		auto testVehicle = std::make_shared<Vehicle>(vehicleDefIt->second, this->organisations[0]);
+		this->city->vehicles.push_back(testVehicle);
+		testVehicle->launch(*this->city, Vec3<float>{x,y,z});
+	}
 }
 
 
