cc3f5e4f8aeadf5f271f244457dea27fb1e9bb00
SupSuper <supsuper@gmail.com>
2015-09-27 18:03:50 +0100

    Minor UI errors

--- game/ufopaedia/ufopaediacategory.cpp
+++ game/ufopaedia/ufopaediacategory.cpp
@@ -178,7 +178,7 @@ void UfopaediaCategory::Update(StageCmd *const cmd)
 void UfopaediaCategory::Render()
 {
 	fw.Stage_GetPrevious(this->shared_from_this())->Render();
-	fw.renderer->drawFilledRect({0, 0}, fw.Display_GetSize(), Colour{0, 0, 0, 128});
+	//fw.renderer->drawFilledRect({0, 0}, fw.Display_GetSize(), Colour{0, 0, 0, 128});
 	menuform->Render();
 	fw.gamecore->MouseCursor->Render();
 }
