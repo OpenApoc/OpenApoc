e661b8471f2d7ba88a30b13b2f367fcbd5a492fc
SupSuper <supsuper@gmail.com>
2015-09-27 18:03:27 +0100

    Added building screen

---
+++ game/city/buildingscreen.cpp
@@ -0,0 +1,65 @@
+
+#include "game/city/buildingscreen.h"
+#include "framework/framework.h"
+
+namespace OpenApoc
+{
+
+BuildingScreen::BuildingScreen(Framework &fw) : Stage(fw)
+{
+	menuform = fw.gamecore->GetForm("FORM_BUILDING_SCREEN");
+}
+
+BuildingScreen::~BuildingScreen() {}
+
+void BuildingScreen::Begin() {}
+
+void BuildingScreen::Pause() {}
+
+void BuildingScreen::Resume() {}
+
+void BuildingScreen::Finish() {}
+
+void BuildingScreen::EventOccurred(Event *e)
+{
+	menuform->EventOccured(e);
+	fw.gamecore->MouseCursor->EventOccured(e);
+
+	if (e->Type == EVENT_KEY_DOWN)
+	{
+		if (e->Data.Keyboard.KeyCode == ALLEGRO_KEY_ESCAPE)
+		{
+			stageCmd.cmd = StageCmd::Command::POP;
+			return;
+		}
+	}
+
+	if (e->Type == EVENT_FORM_INTERACTION && e->Data.Forms.EventFlag == FormEventType::ButtonClick)
+	{
+		if (e->Data.Forms.RaisedBy->Name == "BUTTON_QUIT")
+		{
+			stageCmd.cmd = StageCmd::Command::POP;
+			return;
+		}
+	}
+}
+
+void BuildingScreen::Update(StageCmd *const cmd)
+{
+	menuform->Update();
+	*cmd = this->stageCmd;
+	// Reset the command to default
+	this->stageCmd = StageCmd();
+}
+
+void BuildingScreen::Render()
+{
+	fw.Stage_GetPrevious(this->shared_from_this())->Render();
+	fw.renderer->drawFilledRect({0, 0}, fw.Display_GetSize(), Colour{0, 0, 0, 128});
+	menuform->Render();
+	fw.gamecore->MouseCursor->Render();
+}
+
+bool BuildingScreen::IsTransition() { return false; }
+
+}; // namespace OpenApoc
---
+++ game/city/buildingscreen.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include "framework/stage.h"
+#include "framework/includes.h"
+
+#include "game/resources/gamecore.h"
+#include "game/apocresources/apocresource.h"
+#include "forms/forms.h"
+
+namespace OpenApoc
+{
+
+class BuildingScreen : public Stage
+{
+  private:
+	Form *menuform;
+	StageCmd stageCmd;
+
+  public:
+	BuildingScreen(Framework &fw);
+	~BuildingScreen();
+	// Stage control
+	virtual void Begin() override;
+	virtual void Pause() override;
+	virtual void Resume() override;
+	virtual void Finish() override;
+	virtual void EventOccurred(Event *e) override;
+	virtual void Update(StageCmd *const cmd) override;
+	virtual void Render() override;
+	virtual bool IsTransition() override;
+};
+}; // namespace OpenApoc
