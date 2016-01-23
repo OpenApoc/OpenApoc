55301e7ae2d2855945b25df27b1ccfc168a07ee0
Jonathan Hamilton <jtrhamilton@gmail.com>
2016-01-23 09:59:49 -0800

    Add OpenApoc::mksp<> as an alias for std::make_shared<>

--- library/sp.h
+++ library/sp.h
@@ -11,4 +11,9 @@ template <class T> using sp = std::shared_ptr<T>;
 template <class T> using wp = std::weak_ptr<T>;
 template <class T> using up = std::unique_ptr<T>;
 
+template <typename T, typename... Args> sp<T> mksp(Args &&... args)
+{
+	return std::make_shared<T>(std::forward<Args>(args)...);
+}
+
 } // namespace OpenApoc
