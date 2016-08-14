7236c348e2c8089d89c3f61458be48a5fce34dea
SupSuper <supsuper@gmail.com>
2016-08-14 21:53:06 +0100

    More accuracy, more!

--- game/state/gametime.cpp
+++ game/state/gametime.cpp
@@ -15,7 +15,7 @@ std::locale *GameTime::DATE_FORMAT = nullptr;
 // FIXME: Refactor to always use ptime instead of ticks?
 time_duration GameTime::ticksToPosix(int64_t ticks)
 {
-	int64_t tickTotal = ticks * time_duration::ticks_per_second() / TICKS_PER_SECOND;
+	int64_t tickTotal = std::round(static_cast<double>(ticks * time_duration::ticks_per_second()) / TICKS_PER_SECOND);
 	return time_duration(0, 0, 0, tickTotal);
 }
 
