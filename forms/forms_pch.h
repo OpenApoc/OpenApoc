0043cb9fcedb39794002086730abb4fedbb0de6b
Jonathan Hamilton <jtrhamilton@gmail.com>
2016-10-23 14:14:32 -0700

    Try to add PCH using cotire to cmake

---
+++ forms/forms_pch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "framework/apocresources/apocfont.h"
+#include "framework/configfile.h"
+#include "framework/data.h"
+#include "framework/event.h"
+#include "framework/font.h"
+#include "framework/framework.h"
+#include "framework/image.h"
+#include "framework/keycodes.h"
+#include "framework/logger.h"
+#include "framework/renderer.h"
+#include "framework/sound.h"
+#include "framework/trace.h"
+#include "library/colour.h"
+#include "library/sp.h"
+#include "library/strings.h"
+#include "library/strings_format.h"
+#include "library/vec.h"
+#include <functional>
+#include <list>
+#include <map>
+#include <queue>
+#include <stdexcept>
+#include <tinyxml2.h>
