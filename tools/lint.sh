f956ddb9248714f2d5492bffad69a009343775ab
Jonathan Hamilton <jtrhamilton@gmail.com>
2017-09-05 08:30:45 -0700

    Resurrect travis build, add 'lint' pass
    A recent travis update seems to have broken some packages, so I took
    this opportunity to cleanup some things
    
    Also adds a lint script (tools/lint.sh) that runs clang-format on
    either:
    
    The current git staging area (IE 'git diff --cached') with no arguments:
    ./tools/lint.sh
    The provided revision range:
    ./tools/lint.sh master..branch
    ./tools/lint.sh SHA1..SHA2
    
    No output is required to pass the lint builder. It must be run from the
    root of the git repo, as it queries git for the modified files (and it
    provides them relative to the root)
    
    TODO:
    - Re-add OSX
    - Re-add sanitize builds?
    - Debug builds?
    - Static analysis builds? (clang-tidy?)

---
+++ tools/lint.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+#
+# A clang-format lint script. Usage:
+# ./lint.sh
+#   Runs clang-format on the current git stage
+# ./lint.sh revision..range
+#   Runs clang-format on the specified range of commits
+
+set -eo pipefail
+
+[ -z "${CLANG_FORMAT}" ] && CLANG_FORMAT="clang-format"
+
+if [ -z "$(command -v ${CLANG_FORMAT})" ]; then
+  echo >&2 "clang format binary \"${CLANG_FORMAT}\" not found"
+  exit 1;
+fi;
+
+# Default to 'cached', or the revision passed as an argument
+GIT_REVISION=${1:---cached}
+
+MODIFIED_FILES=$(git diff --name-only --diff-filter=ACMRTUXB ${GIT_REVISION})
+
+
+RET=0
+
+for f in ${MODIFIED_FILES}; do
+  # Skip any non C++ files
+  if ! echo "${f}" | egrep -q "[.](cpp|h)$"; then
+    continue;
+  fi
+
+  OUTPUT=$(${CLANG_FORMAT} ${f} | (diff -u  "${f}" - || true))
+  if [ -n "${OUTPUT}" ]; then
+    echo "ERROR: File \"${f}\" doesn't match expected format, diff:"
+	echo
+	echo "${OUTPUT}"
+	RET=1;
+  fi;
+done;
+
+exit ${RET};
+
