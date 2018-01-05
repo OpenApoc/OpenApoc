5376a229c39215dde1a213fc17b3b8af0c2abca6
redv <rproskurin@gmail.com>
2018-01-06 01:10:10 +0300

    implemented the ScoreScreen

--- game/ui/city/scorescreen.h
+++ game/ui/city/scorescreen.h
@@ -8,17 +8,31 @@ namespace OpenApoc
 
 class GameState;
 class Form;
+class Label;
 
 class ScoreScreen : public Stage
 {
   private:
 	sp<Form> menuform;
+	sp<Form> formScore;
+	sp<Form> formFinance;
+	sp<Label> title;
 
 	sp<GameState> state;
+
+	// The form filling status.
+	bool formScoreFilled = false;
+	bool formFinanceFilled = false;
+
+	// Setup the score mode.
+	void setScoreMode();
+	// Setup the finance mode.
+	void setFinanceMode();
 
   public:
 	ScoreScreen(sp<GameState> state);
 	~ScoreScreen() override;
+
 	// Stage control
 	void begin() override;
 	void pause() override;
