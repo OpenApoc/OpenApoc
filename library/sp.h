159eb78f4cd76173f9b89c5da82169e03d0c765e
Jonathan Hamilton <jtrhamilton@gmail.com>
2018-09-16 10:39:11 -0700

    Update to visual studio 2017

--- library/sp.h
+++ library/sp.h
@@ -2,14 +2,18 @@
 
 #include <memory>
 
-/* A simple alias for sp<T> because I'm wearing out my keyboard */
-
-
+#ifndef _MSC_VER
+/* MSVC doesn't update __cplusplus from c++98's value without special cmdline flags */
 #if __cplusplus < 201703L
 #error Requires at least c++17
 #endif
+#endif
+
 namespace OpenApoc
 {
+
+/* A simple alias for sp<T> because I'm wearing out my keyboard */
+
 
 template <class T> using sp = std::shared_ptr<T>;
 template <class T> using wp = std::weak_ptr<T>;
