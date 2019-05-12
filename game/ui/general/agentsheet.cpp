79c10b1f58d7ebc7a90c38f2fd2dcd4d1be6cee9
Vasiliy Panasenko <37053399+Atrosha@users.noreply.github.com>
2019-05-12 09:02:09 +0300

    finish formatting of tolltips code

--- game/ui/general/agentsheet.cpp
+++ game/ui/general/agentsheet.cpp
@@ -42,25 +42,31 @@ void AgentSheet::display(sp<Agent> item, std::vector<sp<Image>> &ranks, bool tur
 	form->findControlTyped<Graphic>("VALUE_1")->setImage(
 	    createStatsBar(item->initial_stats.health, item->current_stats.health,
 	                   item->modified_stats.health, 100, healthColour, {100, 4}));
-	form->findControlTyped<Graphic>("VALUE_1")->ToolTipText = tr("Health") + ": " + std::to_string(item->modified_stats.health);
-    if (item->current_stats.health != item->modified_stats.health)
-        form->findControlTyped<Graphic>("VALUE_1")->ToolTipText += "/" + std::to_string(item->current_stats.health);
+	form->findControlTyped<Graphic>("VALUE_1")->ToolTipText = tr("Health") + ": " +
+		std::to_string(item->modified_stats.health);
+    	if (item->current_stats.health != item->modified_stats.health)
+        	form->findControlTyped<Graphic>("VALUE_1")->ToolTipText += "/" +
+			std::to_string(item->current_stats.health);
 
 	form->findControlTyped<Label>("LABEL_2")->setText(tr("Accuracy"));
 	form->findControlTyped<Graphic>("VALUE_2")->setImage(
 	    createStatsBar(item->initial_stats.accuracy, item->current_stats.accuracy,
 	                   item->modified_stats.accuracy, 100, accuracyColour, {100, 4}));
-	form->findControlTyped<Graphic>("VALUE_2")->ToolTipText = tr("Accuracy") + ": " + std::to_string(item->modified_stats.accuracy);
-    if (item->current_stats.accuracy != item->modified_stats.accuracy)
-        form->findControlTyped<Graphic>("VALUE_2")->ToolTipText += "/" + std::to_string(item->current_stats.accuracy);
+	form->findControlTyped<Graphic>("VALUE_2")->ToolTipText = tr("Accuracy") + ": " +
+		std::to_string(item->modified_stats.accuracy);
+    	if (item->current_stats.accuracy != item->modified_stats.accuracy)
+        	form->findControlTyped<Graphic>("VALUE_2")->ToolTipText += "/" +
+			std::to_string(item->current_stats.accuracy);
 
 	form->findControlTyped<Label>("LABEL_3")->setText(tr("Reactions"));
 	form->findControlTyped<Graphic>("VALUE_3")->setImage(
 	    createStatsBar(item->initial_stats.reactions, item->current_stats.reactions,
 	                   item->modified_stats.reactions, 100, reactionsColour, {100, 4}));
-	form->findControlTyped<Graphic>("VALUE_3")->ToolTipText = tr("Reactions") + ": " + std::to_string(item->modified_stats.reactions);
-    if (item->current_stats.reactions != item->modified_stats.reactions)
-        form->findControlTyped<Graphic>("VALUE_3")->ToolTipText += "/" + std::to_string(item->current_stats.reactions);
+	form->findControlTyped<Graphic>("VALUE_3")->ToolTipText = tr("Reactions") + ": " +
+			std::to_string(item->modified_stats.reactions);
+    	if (item->current_stats.reactions != item->modified_stats.reactions)
+        	form->findControlTyped<Graphic>("VALUE_3")->ToolTipText += "/" +
+			std::to_string(item->current_stats.reactions);
 
 	form->findControlTyped<Label>("LABEL_4")->setText(turnBased ? tr("Time Units") : tr("Speed"));
 	if (turnBased)
@@ -68,68 +74,84 @@ void AgentSheet::display(sp<Agent> item, std::vector<sp<Image>> &ranks, bool tur
 		form->findControlTyped<Graphic>("VALUE_4")->setImage(
 		    createStatsBar(item->initial_stats.time_units, item->current_stats.time_units,
 		                   item->modified_stats.time_units, 100, speedColour, {100, 4}));
-    	form->findControlTyped<Graphic>("VALUE_4")->ToolTipText = tr("Time Units") + ": " + std::to_string(item->modified_stats.time_units);
-        if (item->current_stats.time_units != item->modified_stats.time_units)
-            form->findControlTyped<Graphic>("VALUE_4")->ToolTipText += "/" + std::to_string(item->current_stats.time_units);
+    		form->findControlTyped<Graphic>("VALUE_4")->ToolTipText = tr("Time Units") + ": " +
+								std::to_string(item->modified_stats.time_units);
+        	if (item->current_stats.time_units != item->modified_stats.time_units)
+            		form->findControlTyped<Graphic>("VALUE_4")->ToolTipText += "/" +
+								std::to_string(item->current_stats.time_units);
 	}
 	else
 	{
 		form->findControlTyped<Graphic>("VALUE_4")->setImage(createStatsBar(
 		    item->initial_stats.getDisplaySpeedValue(), item->current_stats.getDisplaySpeedValue(),
 		    item->modified_stats.getDisplaySpeedValue(), 100, speedColour, {100, 4}));
-    	form->findControlTyped<Graphic>("VALUE_4")->ToolTipText = tr("Speed") + ": " + std::to_string(item->modified_stats.getDisplaySpeedValue());
-        if (item->current_stats.getDisplaySpeedValue() != item->modified_stats.getDisplaySpeedValue())
-            form->findControlTyped<Graphic>("VALUE_4")->ToolTipText += "/" + std::to_string(item->current_stats.getDisplaySpeedValue());
+    		form->findControlTyped<Graphic>("VALUE_4")->ToolTipText = tr("Speed") + ": " +
+			std::to_string(item->modified_stats.getDisplaySpeedValue());
+        	if (item->current_stats.getDisplaySpeedValue() != item->modified_stats.getDisplaySpeedValue())
+            		form->findControlTyped<Graphic>("VALUE_4")->ToolTipText += "/" +
+				std::to_string(item->current_stats.getDisplaySpeedValue());
 	}
 
 	form->findControlTyped<Label>("LABEL_5")->setText(tr("Stamina"));
 	form->findControlTyped<Graphic>("VALUE_5")->setImage(createStatsBar(
 	    item->initial_stats.getDisplayStaminaValue(), item->current_stats.getDisplayStaminaValue(),
 	    item->modified_stats.getDisplayStaminaValue(), 100, staminaColour, {100, 4}));
-	form->findControlTyped<Graphic>("VALUE_5")->ToolTipText = tr("Stamina") + ": " + std::to_string(item->modified_stats.getDisplayStaminaValue());
-    if (item->current_stats.getDisplayStaminaValue() != item->modified_stats.getDisplayStaminaValue())
-        form->findControlTyped<Graphic>("VALUE_5")->ToolTipText += "/" + std::to_string(item->current_stats.getDisplayStaminaValue());
+	form->findControlTyped<Graphic>("VALUE_5")->ToolTipText = tr("Stamina") + ": " +
+		std::to_string(item->modified_stats.getDisplayStaminaValue());
+    	if (item->current_stats.getDisplayStaminaValue() != item->modified_stats.getDisplayStaminaValue())
+        	form->findControlTyped<Graphic>("VALUE_5")->ToolTipText += "/" +
+			std::to_string(item->current_stats.getDisplayStaminaValue());
 
-    form->findControlTyped<Label>("LABEL_6")->setText(tr("Bravery"));
+	form->findControlTyped<Label>("LABEL_6")->setText(tr("Bravery"));
 	form->findControlTyped<Graphic>("VALUE_6")->setImage(
 	    createStatsBar(item->initial_stats.bravery, item->current_stats.bravery,
 	                   item->modified_stats.bravery, 100, braveryColour, {100, 4}));
-	form->findControlTyped<Graphic>("VALUE_6")->ToolTipText = tr("Bravery") + ": " + std::to_string(item->modified_stats.bravery);
-    if (item->current_stats.bravery != item->modified_stats.bravery)
-        form->findControlTyped<Graphic>("VALUE_6")->ToolTipText += "/" + std::to_string(item->current_stats.bravery);
+	form->findControlTyped<Graphic>("VALUE_6")->ToolTipText = tr("Bravery") + ": " +
+		std::to_string(item->modified_stats.bravery);
+    	if (item->current_stats.bravery != item->modified_stats.bravery)
+        	form->findControlTyped<Graphic>("VALUE_6")->ToolTipText += "/" +
+			std::to_string(item->current_stats.bravery);
 
 	form->findControlTyped<Label>("LABEL_7")->setText(tr("Strength"));
 	form->findControlTyped<Graphic>("VALUE_7")->setImage(
 	    createStatsBar(item->initial_stats.strength, item->current_stats.strength,
 	                   item->modified_stats.strength, 100, strengthColour, {100, 4}));
-	form->findControlTyped<Graphic>("VALUE_7")->ToolTipText = tr("Strength") + ": " + std::to_string(item->modified_stats.strength);
-    if (item->current_stats.strength != item->modified_stats.strength)
-        form->findControlTyped<Graphic>("VALUE_7")->ToolTipText += "/" + std::to_string(item->current_stats.strength);
+	form->findControlTyped<Graphic>("VALUE_7")->ToolTipText = tr("Strength") + ": " +
+		std::to_string(item->modified_stats.strength);
+    	if (item->current_stats.strength != item->modified_stats.strength)
+        	form->findControlTyped<Graphic>("VALUE_7")->ToolTipText += "/" +
+			std::to_string(item->current_stats.strength);
 
 	form->findControlTyped<Label>("LABEL_8")->setText(tr("Psi-energy"));
 	form->findControlTyped<Graphic>("VALUE_8")->setImage(
 	    createStatsBar(item->initial_stats.psi_energy, item->current_stats.psi_energy,
 	                   item->modified_stats.psi_energy, 100, psiEnergyColour, {100, 4}));
-	form->findControlTyped<Graphic>("VALUE_8")->ToolTipText = tr("Psi-energy") + ": " + std::to_string(item->modified_stats.psi_energy);
-    if (item->current_stats.psi_energy != item->modified_stats.psi_energy)
-        form->findControlTyped<Graphic>("VALUE_8")->ToolTipText += "/" + std::to_string(item->current_stats.psi_energy);
+	form->findControlTyped<Graphic>("VALUE_8")->ToolTipText = tr("Psi-energy") + ": " +
+		std::to_string(item->modified_stats.psi_energy);
+    	if (item->current_stats.psi_energy != item->modified_stats.psi_energy)
+        	form->findControlTyped<Graphic>("VALUE_8")->ToolTipText += "/" +
+			std::to_string(item->current_stats.psi_energy);
 
 	form->findControlTyped<Label>("LABEL_9")->setText(tr("Psi-attack"));
 	form->findControlTyped<Graphic>("VALUE_9")->setImage(
 	    createStatsBar(item->initial_stats.psi_attack, item->current_stats.psi_attack,
 	                   item->modified_stats.psi_attack, 100, psiAttackColour, {100, 4}));
-	form->findControlTyped<Graphic>("VALUE_9")->ToolTipText = tr("Psi-attack") + ": " + std::to_string(item->modified_stats.psi_attack);
-    if (item->current_stats.psi_attack != item->modified_stats.psi_attack)
-        form->findControlTyped<Graphic>("VALUE_9")->ToolTipText += "/" + std::to_string(item->current_stats.psi_attack);
+	form->findControlTyped<Graphic>("VALUE_9")->ToolTipText = tr("Psi-attack") + ": " +
+		std::to_string(item->modified_stats.psi_attack);
+    	if (item->current_stats.psi_attack != item->modified_stats.psi_attack)
+        	form->findControlTyped<Graphic>("VALUE_9")->ToolTipText += "/" +
+			std::to_string(item->current_stats.psi_attack);
 
 	form->findControlTyped<Label>("LABEL_10")->setText(tr("Psi-defence"));
 	form->findControlTyped<Graphic>("VALUE_10")
 	    ->setImage(createStatsBar(item->initial_stats.psi_defence, item->current_stats.psi_defence,
 	                              item->modified_stats.psi_defence, 100, psiDefenceColour,
 	                              {100, 4}));
-	form->findControlTyped<Graphic>("VALUE_10")->ToolTipText = tr("Psi-defence") + ": " + std::to_string(item->modified_stats.psi_defence);
-    if (item->current_stats.psi_defence != item->modified_stats.psi_defence)
-        form->findControlTyped<Graphic>("VALUE_10")->ToolTipText += "/" + std::to_string(item->current_stats.psi_defence);
+	form->findControlTyped<Graphic>("VALUE_10")->ToolTipText = tr("Psi-defence") + ": " +
+		std::to_string(item->modified_stats.psi_defence);
+    	if (item->current_stats.psi_defence != item->modified_stats.psi_defence)
+        	form->findControlTyped<Graphic>("VALUE_10")->ToolTipText += "/" +
+			std::to_string(item->current_stats.psi_defence);
 }
 
 void AgentSheet::clear()
