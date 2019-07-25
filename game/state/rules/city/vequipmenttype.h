2b67842a712b5f21eeb39d9172519fe82a7766f6
Jonathan Hamilton <jtrhamilton@gmail.com>
2019-07-25 11:48:44 -0700

    VEquipmentType::equipscreen_size wasn't being set in the default constructor
    Also changes to use member initializers instead of an initializer list in the
    constructor, so hopefully less error prone and more obvious if things are missed

--- game/state/rules/city/vequipmenttype.h
+++ game/state/rules/city/vequipmenttype.h
@@ -23,7 +23,7 @@ class VEquipmentType : public StateObject
 {
 	STATE_OBJECT(VEquipmentType)
   public:
-	VEquipmentType();
+	VEquipmentType() = default;
 
 	enum class User
 	{
@@ -35,39 +35,39 @@ class VEquipmentType : public StateObject
 	~VEquipmentType() override = default;
 
 	// Shared stuff
-	EquipmentSlotType type;
+	EquipmentSlotType type = EquipmentSlotType::VehicleGeneral;
 	UString id;
 	UString name;
-	int weight;
-	int max_ammo;
+	int weight = 0;
+	int max_ammo = 0;
 	StateRef<VAmmoType> ammo_type;
 	sp<Image> equipscreen_sprite;
-	Vec2<int> equipscreen_size;
+	Vec2<int> equipscreen_size = {0, 0};
 	StateRef<Organisation> manufacturer;
-	int store_space;
+	int store_space = 0;
 	std::set<User> users;
 
 	// Weapons
-	int speed;
+	int speed = 0;
 	std::list<sp<Image>> projectile_sprites; // A list of sprites forming the projectile
 	                                         // 'bullet'/'beam' - 'nullptr' gaps are expected
-	int damage;
-	int accuracy;
+	int damage = 0;
+	int accuracy = 0;
 	// Fire delay, in ticks, to fire a shot
-	int fire_delay;
-	int tail_size;
-	bool guided;
+	int fire_delay = 0;
+	int tail_size = 0;
+	bool guided = false;
 	// How much can it turn per tick.
 	// Based on the fact that retribution (tr = 10) turns 90 degrees (PI/2) per second
 	// One point of turn rate is equal to PI/20 turned per second
-	int turn_rate;
+	int turn_rate = 0;
 	// Divide by 32 to get range in tiles
-	int range;
+	int range = 0;
 	// Projectile's ttl, in voxels travelled
 	float ttl = 0.0f;
-	int firing_arc_1;
-	int firing_arc_2;
-	bool point_defence;
+	int firing_arc_1 = 0;
+	int firing_arc_2 = 0;
+	bool point_defence = false;
 	sp<Sample> fire_sfx;
 	sp<Sample> impact_sfx;
 	StateRef<DoodadType> explosion_graphic;
@@ -78,18 +78,18 @@ class VEquipmentType : public StateObject
 	std::list<StateRef<VEquipmentType>> splitIntoTypes;
 
 	// Engine stuff
-	int power;
-	int top_speed;
+	int power = 0;
+	int top_speed = 0;
 
 	// Other ('general') equipment stuff
-	int accuracy_modifier;
-	int cargo_space;
-	int passengers;
-	int alien_space;
-	int missile_jamming;
-	int shielding;
-	bool cloaking;
-	bool teleporting;
+	int accuracy_modifier = 0;
+	int cargo_space = 0;
+	int passengers = 0;
+	int alien_space = 0;
+	int missile_jamming = 0;
+	int shielding = 0;
+	bool cloaking = false;
+	bool teleporting = false;
 	bool dimensionShifting = false;
 
 	// Score requirement
