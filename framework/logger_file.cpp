89a8fb74c10f0133381144ea088b236ac3b57619
Jonathan Hamilton <jtrhamilton@gmail.com>
2019-07-31 20:41:58 -0700

    Print backtrace to file again
    Defaults to on LogLevel::Error or greater

--- framework/logger_file.cpp
+++ framework/logger_file.cpp
@@ -14,39 +14,53 @@ ConfigOptionInt fileLogLevelOption(
     "Logger", "FileLevel",
     "Loglevel to output to file (0 = nothing, 1 = error, 2 = warning, 3 = info, 4 = debug)", 3);
 
+ConfigOptionInt backtraceLogLevelOption("Logger", "BacktraceLevel",
+                                        "Loglevel to print a backtrace to file log (0 = nothing, 1 "
+                                        "= error, 2 = warning, 3 = info, 4 = debug)",
+                                        1);
+
 LogFunction previousFunction; // To allow chaining log functions
 
 LogLevel fileLogLevel = LogLevel::Nothing;
+LogLevel backtraceLogLevel = LogLevel::Nothing;
 std::ofstream logFile;
 
 void FileLogFunction(LogLevel level, UString prefix, const UString &text)
 {
 	previousFunction(level, prefix, text);
-	if (level > fileLogLevel)
+	if (level <= fileLogLevel)
 	{
-		return;
+		UString levelPrefix;
+		switch (level)
+		{
+			case LogLevel::Error:
+				levelPrefix = "E";
+				break;
+			case LogLevel::Warning:
+				levelPrefix = "W";
+				break;
+			case LogLevel::Info:
+				levelPrefix = "I";
+				break;
+			case LogLevel::Debug:
+				levelPrefix = "D";
+				break;
+			default:
+				levelPrefix = "U";
+				break;
+		}
+		auto message = OpenApoc::format("%s %s: %s", levelPrefix, prefix, text);
+		logFile << message << std::endl;
 	}
-	UString levelPrefix;
-	switch (level)
+
+	if (level <= backtraceLogLevel)
 	{
-		case LogLevel::Error:
-			levelPrefix = "E";
-			break;
-		case LogLevel::Warning:
-			levelPrefix = "W";
-			break;
-		case LogLevel::Info:
-			levelPrefix = "I";
-			return;
-		case LogLevel::Debug:
-			levelPrefix = "D";
-			return;
-		default:
-			levelPrefix = "U";
-			break;
+		auto backtrace = getBacktrace();
+		for (const auto &frame : backtrace)
+		{
+			logFile << frame << std::endl;
+		}
 	}
-	auto message = OpenApoc::format("%s %s: %s", levelPrefix, prefix, text);
-	logFile << message << std::endl;
 }
 
 } // namespace
@@ -60,6 +74,7 @@ void enableFileLogger(const char *outputFile)
 		LogError("File logger failed to open file \"%s\"", outputFile);
 	}
 	fileLogLevel = (LogLevel)fileLogLevelOption.get();
+	backtraceLogLevel = (LogLevel)backtraceLogLevelOption.get();
 	previousFunction = getLogCallback();
 	setLogCallback(FileLogFunction);
 }
