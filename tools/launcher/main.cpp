5819076e4512ccb874bdd66ce9c328f0dc4cb476
Jonathan Hamilton <jtrhamilton@gmail.com>
2019-09-02 17:38:20 -0700

    Don't use SDL_Main in the launcher
    This conflicts with QT's own WinMain implementation

--- tools/launcher/main.cpp
+++ tools/launcher/main.cpp
@@ -1,9 +1,13 @@
+// WinMain is already defined in qt, so we don't want to accidently include the
+// SDL one
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
 #include "launcherwindow.h"
 #include <QApplication>
-#include <SDL_main.h>
-
 int main(int argc, char **argv)
 {
+	SDL_SetMainReady();
 	QApplication LauncherApp(argc, argv);
 	LauncherWindow Launcher;
 	Launcher.show();
