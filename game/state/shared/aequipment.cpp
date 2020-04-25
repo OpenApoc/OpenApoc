f4cc7fc02e8d3b6c9e509cdca7d30f6b13369566
FilmBoy84 <33913088+FilmBoy84@users.noreply.github.com>
2020-04-25 12:51:59 +0100

    Perform CLANG Format
    Forgot 'cos im a fool

--- game/state/shared/aequipment.cpp
+++ game/state/shared/aequipment.cpp
@@ -826,11 +826,11 @@ void AEquipment::fire(GameState &state, Vec3<float> targetPosition, StateRef<Bat
 	if (!config().getBool("OpenApoc.Cheat.InfiniteAmmo") ||
 	    this->ownerAgent->owner != state.getPlayer())
 	{
-			if (this->ammo < number_of_shots)
-			{
-				number_of_shots = this->ammo;
-			}
-			this->ammo -= number_of_shots;
+		if (this->ammo < number_of_shots)
+		{
+			number_of_shots = this->ammo;
+		}
+		this->ammo -= number_of_shots;
 	}
 	if (ammo == 0 && payloadType)
 	{
