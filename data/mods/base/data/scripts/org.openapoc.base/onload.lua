94b9f86500c9bde18a66c10751e50874667f7b9a
Jonathan Hamilton <jtrhamilton@gmail.com>
2020-08-26 18:02:52 -0700

    Extend the base mod's onload script to read language-specific GameState patches
    The plan here is to keep translated GameState resources (e.g. UFOPaedia
    entries, names of equipment/vehicles/corps/buildings in there)

--- data/mods/base/data/scripts/org.openapoc.base/onload.lua
+++ data/mods/base/data/scripts/org.openapoc.base/onload.lua
@@ -5,9 +5,29 @@ local FW = OpenApoc.Framework
 
 FW.LogInfo ("On load script called")
 
-local gamestateString = "submods/org.openapoc.base/difficulty" .. tostring(GS.difficulty)
-FW.LogInfo("Loading difficulty patch" .. gamestateString)
+local gamestateDifficultyString = "submods/org.openapoc.base/difficulty" .. tostring(GS.difficulty)
+FW.LogInfo("Loading difficulty patch" .. gamestateDifficultyString)
 
-GS.appendGameState(GS, gamestateString)
+GS.appendGameState(GS, gamestateDifficultyString)
 
 FW.LogInfo("Finished loading difficulty patch")
+
+local gamestateLanguageString = "submods/org.openapoc.base/language/" .. tostring(FW.getLanguageCountry())
+FW.LogInfo("Loading language patch" .. gamestateLanguageString)
+local success = GS.appendGameState(GS, gamestateLanguageString)
+if success == true then
+  FW.LogInfo("Finished loading language patch")
+else
+  FW.LogInfo("No country-specific language patch found");
+  gamestateLanguageString = "submods/org.openapoc.base/language/" .. tostring(FW.getLanguage())
+  FW.LogInfo("Loading language patch" .. gamestateLanguageString)
+  success = GS.appendGameState(GS, gamestateLanguageString)
+  if success == true then
+    FW.LogInfo("Finished loading langauge patch")
+  else
+    FW.LogInfo("No langauge patch found")
+  end
+
+end
+
+
