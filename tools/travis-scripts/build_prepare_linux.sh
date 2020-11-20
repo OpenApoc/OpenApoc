ba89b83be49fc06fb2481f7cdb01c0b108c12c31
Jonathan Hamilton <jtrhamilton@gmail.com>
2020-11-19 23:18:25 -0800

    Fixup travis script

--- tools/travis-scripts/build_prepare_linux.sh
+++ tools/travis-scripts/build_prepare_linux.sh
@@ -6,19 +6,6 @@ echo "Setting ccache variables"
 ccache -M 1G
 ccache -s
 
-if [ "$PASS" = "build" ]; then
-  if [ "$CXX" = "clang++-10" ]; then
-    sudo apt-get install clang-10
-  fi
-  if [ "$CXX" = "gcc-10" ]; then
-    sudo apt-get install gcc-10
-  fi
-fi
-
-if [ "$PASS" = "lint" ]; then
-  sudo apt-get install clang-format clang-tidy
-fi
-
 echo "Installing dependencies"
 sudo apt-get install libsdl2-dev cmake build-essential git libunwind8-dev libboost-locale-dev libboost-filesystem-dev libboost-program-options-dev qtbase5-dev libvorbis-dev
 
