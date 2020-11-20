64285b9d05206db8c114ffec64615a1da716dd83
Jonathan Hamilton <jtrhamilton@gmail.com>
2020-11-19 22:53:47 -0800

    Match readme and travis dependencies
    Re-orders them, adds cmake explicitly to travis and removes libegl1/libgles2

--- tools/travis-scripts/build_prepare_linux.sh
+++ tools/travis-scripts/build_prepare_linux.sh
@@ -7,7 +7,8 @@ ccache -M 1G
 ccache -s
 
 echo "Installing dependencies"
-sudo apt-get install libunwind8-dev libsdl2-dev libboost-locale-dev libboost-filesystem-dev libboost-program-options-dev libegl1-mesa-dev libgles2-mesa-dev qtbase5-dev libvorbis-dev -y
+sudo apt-get install libsdl2-dev cmake build-essential git libunwind8-dev libboost-locale-dev libboost-filesystem-dev libboost-program-options-dev qtbase5-dev libvorbis-dev
+
 
 echo "Fetching minimal cd.iso for build"
 wget http://s2.jonnyh.net/pub/cd_minimal.iso.xz -O data/cd.iso.xz
