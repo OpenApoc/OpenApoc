c5efedd03641230940dca3c22da86791face08cb
Jonathan Hamilton <jtrhamilton@gmail.com>
2022-09-25 14:33:07 +0100

    Use invoke_result_t instead  of result_of<>::type (#1104)
    std::result_of<> has been deprecated in c++17

--- framework/framework.h
+++ framework/framework.h
@@ -103,9 +103,9 @@ class Framework
 	// add new work item to the pool
 	template <class F, class... Args>
 	auto threadPoolEnqueue(F &&f, Args &&...args)
-	    -> std::shared_future<typename std::result_of<F(Args...)>::type>
+	    -> std::shared_future<typename std::invoke_result_t<F, Args...>>
 	{
-		using return_type = typename std::result_of<F(Args...)>::type;
+		using return_type = typename std::invoke_result_t<F, Args...>;
 
 		auto task = std::make_shared<std::packaged_task<return_type()>>(
 		    std::bind(std::forward<F>(f), std::forward<Args>(args)...));
