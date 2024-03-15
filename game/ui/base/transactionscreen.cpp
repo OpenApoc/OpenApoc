396dcece47ba9128b70903522a8c92aa99dfda9e
Ayrton Denner <ayrtondenner_2013@hotmail.com>
2024-03-14 22:28:35 -0300

    Removing Brainsucker pod from alien containment list

--- game/ui/base/transactionscreen.cpp
+++ game/ui/base/transactionscreen.cpp
@@ -386,6 +386,7 @@ void TransactionScreen::populateControlsAlien()
 {
 	// TODO: Fix why aliens are being shown twice
 	// TODO: Show only rows for aliens that exists in this base!
+
 	int leftIndex = getLeftIndex();
 	int rightIndex = getRightIndex();
 	for (auto &ae : state->agent_equipment)
@@ -397,7 +398,7 @@ void TransactionScreen::populateControlsAlien()
 		// Add alien
 		for (auto &b : state->player_bases)
 		{
-			if (b.second->inventoryBioEquipment[ae.first] > 0)
+			if (b.second->inventoryBioEquipment[ae.first] > 0 && ae.first != "AEQUIPMENTTYPE_BRAINSUCKER_POD")
 			{
 				auto control = TransactionControl::createControl(
 				    *state, StateRef<AEquipmentType>{state.get(), ae.first}, leftIndex, rightIndex);
