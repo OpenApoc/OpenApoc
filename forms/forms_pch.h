b47bc80cba9edb92c1e3ed5563e8b26914d11009
Jonathan Hamilton <jtrhamilton@gmail.com>
2025-02-27 20:16:48 +0000

    Quick hacky resurrection of PCH
    The StateRef<T>::get() and similar templates might need reworking, including something that instantiates those before the definition is invalid, and that seems more likely to happen with PCHs and restricts the things we can precompile.

--- forms/forms_pch.h
+++ forms/forms_pch.h
@@ -2,25 +2,10 @@
 
 #include "dependencies/pugixml/src/pugixml.hpp"
 #include "forms/control.h"
-#include "framework/apocresources/apocfont.h"
-#include "framework/configfile.h"
+#include "forms/forms_enums.h"
 #include "framework/data.h"
 #include "framework/event.h"
-#include "framework/font.h"
 #include "framework/framework.h"
 #include "framework/image.h"
-#include "framework/keycodes.h"
-#include "framework/logger.h"
 #include "framework/renderer.h"
-#include "framework/sound.h"
-#include "framework/trace.h"
-#include "library/colour.h"
 #include "library/sp.h"
-#include "library/strings.h"
-#include "library/strings_format.h"
-#include "library/vec.h"
-#include <functional>
-#include <list>
-#include <map>
-#include <queue>
-#include <stdexcept>
