c29f172a2a3fdea42a8eeb2396911d20ee353369
Jonathan Hamilton <jtrhamilton@gmail.com>
2026-03-24 15:06:49 -0700

    Update glm from fork v0.9.9.8 to upstream v1.0.3
    - Add global GLM_ENABLE_EXPERIMENTAL define (required for gtx headers in 1.0.x)
    - Remove GLM_FORCE_RADIANS (radians-only since 0.9.6, define removed in 1.0.x)
    - Update version check to static_assert(GLM_VERSION_MAJOR >= 1)

--- library/vec.h
+++ library/vec.h
@@ -1,23 +1,17 @@
 #pragma once
 
-/* GLM_GTX_transform is an experimental extension now, apparently */
-#define GLM_ENABLE_EXPERIMENTAL
-
-#define GLM_FORCE_RADIANS
 #include <fmt/format.h>
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <ostream>
 
+static_assert(GLM_VERSION_MAJOR >= 1, "GLM 1.0 or above is required");
+
 namespace OpenApoc
 {
-#if (GLM_VERSION_MAJOR == 0 && GLM_VERSION_MINOR <= 9 && GLM_VERSION_PATCH <= 5)
-#error GLM 0.9.6 or above is required
-#else
 template <typename T> using Vec3 = glm::vec<3, T, glm::highp>;
 
 template <typename T> using Vec2 = glm::vec<2, T, glm::highp>;
-#endif
 
 static inline float mix(float a, float b, float factor) { return a * (1.0f - factor) + b * factor; }
 
